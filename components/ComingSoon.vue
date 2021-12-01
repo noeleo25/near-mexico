d432f31b1f93f317bbe6f1c7d1eccccbf0402938
Noemi Leon <noemi.telematik213@gmail.com>
2021-11-30 19:10:29 -0500

    coming soon

---
+++ components/ComingSoon.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="cs-bg">
+        <img src="~/assets/map.svg">
+    </div>
+</template>
+
+<style>
+html{
+    background-color: black;
+}
+.cs-bg{
+    color: #fff;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+}
+</style>
