1e4c2ec29bf4027f7f687f6c55780a1713fde3a0
Jommp <jommp.96@gmail.com>
2022-01-20 12:14:40 -0500

    Feat: Add animations with css and js
    Edit:
    - Banner, CommunityNear components and community-near, mexico-banner images. Add animations with css and js
    
    Related: ST-97, ST-100

--- components/sections/Banner.vue
+++ components/sections/Banner.vue
@@ -6,7 +6,13 @@
     rightStyle="img-col d-none d-lg-block"
   >
     <template v-slot:left-side >
-      <h1 class="mb-0"><span class="text-uppercase">Near</span> México</h1>
+      <h1 
+        class="typewrite" 
+        data-period="2000" 
+        data-type='[ "NEAR México" ]'
+      >
+        <span class="wrap"></span>
+      </h1>
       <p>La voz de la comunidad para el ecosistema NEAR en México</p>
       <div>
         <button class="join-btn mr-2">Unirse</button>
@@ -34,6 +40,66 @@ export default {
   components: {
     PlayIcon
   },
+  mounted () {
+    this.typeAnimation()
+  },
+  methods: {
+    typeAnimation() {
+      var elements = document.getElementsByClassName('typewrite')
+      for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type')
+        var period = elements[i].getAttribute('data-period')
+        if (toRotate) {
+          new txtType(elements[i], JSON.parse(toRotate), period)
+        }
+      }
+      //CSS
+      var css = document.createElement("style")
+      css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid" + "" + "}"
+      document.body.appendChild(css)
+    }
+  }
+}
+var txtType = function(el, toRotate, period) {
+  this.toRotate = toRotate
+  this.el = el
+  this.loopNum = 0
+  this.period = parseInt(period, 10) || 2000
+  this.txt = ''
+  this.tick()
+  this.isDeleting = false
+}
+
+txtType.prototype.tick = function() {
+
+  var i = this.loopNum % this.toRotate.length
+  var fullTxt = this.toRotate[i]
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1)
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1)
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>'
+
+  var that = this
+  var delta = 200 - Math.random() * 100
+
+  if (this.isDeleting) { 
+    delta /= 2
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period
+    this.isDeleting = true
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false
+    this.loopNum++
+    delta = 500
+  }
+
+  setTimeout(() => { that.tick() }, delta)
 }
 </script>
 
@@ -47,7 +113,7 @@ export default {
   font-weight: 600;
 }
 .text-col .join-btn {
-  width: 116px;
+  width: 120px;
   height: 42px;
   border: 0;
   border-radius: 6px;
@@ -55,6 +121,31 @@ export default {
   background-color: #29c44d;
   font-size: 1rem;
   color: #FFFFFF;
+}
+.join-btn::after {
+  content: '';
+  width: 5px; 
+  height: 5px;
+  border-radius: 100%;
+  border: 6px solid #29c44d;
+  position: absolute;
+  z-index: -1;
+  top: 63%;
+  left: 13%;
+  transform: translate(-50%, -50%);
+  animation: ring 1.5s infinite;
+}
+@keyframes ring {
+  0% {
+    width: 30px;
+    height: 30px;
+    opacity: 1;
+  }
+  100% {
+    width: 150px;
+    height: 150px;
+    opacity: 0;
+  }
 }
 .text-col .welcome-btn {
   width: 140px;
--- components/sections/CommunityNear.vue
+++ components/sections/CommunityNear.vue
@@ -12,14 +12,14 @@
 
     <template v-slot:right-side >
       <h2 class="mb-0 section-title">Comunidad de Near México</h2>
-      <p class="text-justify">
+      <p class="text-justify first-paragraph">
         Somos un grupo de entusiastas Mexicanos conformados por 
         distintos perfiles profesionales: creadores, colaboradores, 
         emprendedores e iniciales los cuales tenemos como objetivo la 
         difusión y apoyo en el trabajo de la tecnología blockchain.
       </p>
 
-      <p class="text-justify">
+      <p class="text-justify second-paragraph">
         Si quieres pasar un buen rato compartiendo y aprendiendo 
         conocimiento  te invitamos a unirte a esta gran comunidad. 
       </p>
@@ -39,13 +39,56 @@ export default {
 </script>
 
 <style scoped>
+.left-col img {
+  animation: slideToLeft 2.5s ease;
+}
+
+.section-title {
+  animation: slideToRight 1.5s ease;
+}
+
 .right-col p {
   margin-bottom: 2rem;
+}
+
+.first-paragraph {
+  animation: slideToRight 2s ease;
+}
+
+.second-paragraph {
+  animation: slideToRight 2.5s ease;
 }
 
 .social-media {
   max-width: 540px;
   justify-content: center;
+  
+  animation: slideToRight 3s ease;
 }
 
+@keyframes slideToRight {
+  0% {
+    opacity: 0;
+    transform: translate(-100px, 0);
+  }
+  25% {
+    opacity: .5;
+  }
+  100% {
+    transform: translate( 0, 0);
+  }
+}
+
+@keyframes slideToLeft {
+  0% {
+    opacity: 0;
+    transform: translate(100px, 0) scale(0.5);
+  }
+  50% {
+    transform: scale(1);
+  }
+  100% {
+    transform: translate( 0, 0);
+  }
+}
 </style>
